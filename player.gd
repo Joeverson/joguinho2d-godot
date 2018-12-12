117e6e213ad8991076aed4893b53a6b0da8630ce
Joerverson Santos <joerverson.santos@gmail.com>
2018-12-12 01:33:34 -0300

    init

--- player.gd
+++ player.gd
@@ -1,30 +1,37 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var speed = 500
+
+const UP = Vector2(0, -1)
+const GRAVITY = 20
+const SPEED = 200
+const INITIAL_SPEED = 0
+const JUMP_HEIGHT = -400
 
 var velocity = Vector2()
 
 func get_input():
-    velocity = Vector2(0, 500)
+    #Gravity
+    velocity.y += GRAVITY
 	
-    if Input.is_action_pressed('ui_right'):
-        get_node("player").set_flip_h(false)
-        velocity.x += 1
+    if Input.is_action_pressed('ui_right'):        
+        get_node("lion").set_flip_h(false)
+        velocity.x = SPEED
 		
-    if Input.is_action_pressed('ui_left'):
-        get_node("player").set_flip_h(true)
-        velocity.x -= 1
-		
-    if Input.is_action_pressed('ui_down'):
-        velocity.y += 1
-		
-    if Input.is_action_pressed('ui_up'):
-        velocity.y -= 1
-		
-    velocity = velocity.normalized() * speed
+    elif Input.is_action_pressed('ui_left'):
+        get_node("lion").set_flip_h(true)   
+        velocity.x = -SPEED
+    else:
+	    velocity.x = INITIAL_SPEED
+
+    if is_on_floor():
+	    if Input.is_action_pressed('ui_up'):
+		    velocity.y = JUMP_HEIGHT
+
+    #velocity = velocity.normalized() * speed
 
 
 func _physics_process(delta):
     get_input()
-    move_and_slide(velocity)
+    velocity = move_and_slide(velocity, UP)
 	
